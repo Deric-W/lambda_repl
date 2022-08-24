cf339ec6c56393518e4c901dcdd190be166e5c1c
Eric Wolf <robo-eric@gmx.de>
2022-08-24 20:37:58 +0200

    add empty python project

---
+++ lambda_repl/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""REPL for the lambda calculus"""
+
+__version__ = "0.0.1"
+__author__  = "Eric Niklas Wolf"
+__email__   = "eric_niklas.wolf@mailbox.tu-dresden.de"
+__all__ = ()
